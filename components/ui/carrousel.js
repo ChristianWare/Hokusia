405550a299151e3582aac78cbc4560cdb2a36fb6
Chris <chris.ware.dev@gmail.com>
2021-08-16 23:29:06 -0700

    Loading json and slider

---
+++ components/ui/carrousel.js
@@ -0,0 +1,36 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import Slider from "react-slick";
+
+const CarrouselComp = (props) => {
+  // Carrousel Settings, to be used inside of the Slider('react-slick')
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div>
+      <Slider {...settings}>
+        {props.data.map((slide) => (
+          <div key={slide.id}>
+            <div
+              className='carrousel_wrapper'
+              style={{
+                background: `url(/images/arts/${slide.name}) no-repeat`,
+              }}
+            >
+              <div>{slide.title}</div>
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default CarrouselComp;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
-const Home = () => {
+import { getJsonData } from "../utils/tools"
+import CarrouselComp from "../components/ui/carrousel"
+
+const Home = ({data}) => {
+
+console.log({data})
+
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <CarrouselComp data={data.carrousel}/>
+    </>
   )
+}
+
+export const getStaticProps = async() => {
+  const data = await getJsonData();
+
+  return{
+    props:{
+      data: data
+    }
+  }
 }
 
 export default Home
